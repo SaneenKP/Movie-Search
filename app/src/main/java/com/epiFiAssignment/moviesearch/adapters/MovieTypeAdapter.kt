b6d16c1aa4e398712169fd8f2f583112653a1f60
Saneen K P <saneenkp2000@gmail.com>
2022-09-30 00:50:52 +0530

    movie details view hald implemented

---
+++ app/src/main/java/com/epiFiAssignment/moviesearch/adapters/MovieTypeAdapter.kt
@@ -0,0 +1,26 @@
+package com.epiFiAssignment.moviesearch.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuView
+import androidx.recyclerview.widget.RecyclerView
+
+class MovieTypeAdapter(
+) : RecyclerView.Adapter<MovieTypeAdapter.MovieTypeViewHolder>() {
+
+    inner class MovieTypeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieTypeViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MovieTypeViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
