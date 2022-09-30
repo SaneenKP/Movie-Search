87d02143d43218e1826aa7fe241d28dbf63644df
Saneen K P <saneenkp2000@gmail.com>
2022-09-30 18:01:03 +0530

    retrofit interface methods added

---
+++ app/src/main/java/com/epiFiAssignment/moviesearch/retrofit/MoviesApiService.kt
@@ -0,0 +1,16 @@
+package com.epiFiAssignment.moviesearch.retrofit
+
+import com.epiFiAssignment.moviesearch.models.Movie
+import com.epiFiAssignment.moviesearch.models.SearchResult
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MoviesApiService {
+
+    @GET
+    suspend fun searchMovie(@Query("s") searchString : String , @Query("page") page : Int , @Query("type") type : String) : SearchResult
+
+    @GET
+    suspend fun getMovie(@Query("i") movieId : String) : Movie
+
+}
