f10b6b6f13adc580b9c362f7d230c241150d0f09
Saneen K P <saneenkp2000@gmail.com>
2022-10-03 18:17:24 +0530

    movie dao implemented

--- app/src/main/java/com/epiFiAssignment/moviesearch/db/dao/MovieDao.kt
+++ app/src/main/java/com/epiFiAssignment/moviesearch/db/dao/MovieDao.kt
@@ -4,19 +4,21 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.epiFiAssignment.moviesearch.models.Movie
-import com.epiFiAssignment.moviesearch.models.RoomMovieData
+import com.epiFiAssignment.moviesearch.models.BookmarkMovieData
 import com.epiFiAssignment.moviesearch.utils.Constants
 
 @Dao
 interface MovieDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addMovies(movies : RoomMovieData)
+    suspend fun addMovies(movies : BookmarkMovieData)
 
     @Query("SELECT imdbID FROM ${Constants.MOVIE_TABLE_NAME}")
     suspend fun getMovieKeys() : List<String>
 
     @Query("DELETE FROM ${Constants.MOVIE_TABLE_NAME} WHERE imdbID = :movieId ")
     suspend fun deleteMovie(movieId : String)
+
+    @Query("SELECT * FROM ${Constants.MOVIE_TABLE_NAME}")
+    suspend fun getBookmarkedMovies() : List<BookmarkMovieData>
 }
